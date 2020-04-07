ae7caef594955393e1d1acba2cbb1119eff2fb46
Oleg Mitrakhovich <o.mitrakhovich@binge.com>
2020-04-07 12:30:26 -0400

    adding Xcode project

---
+++ BYTZ/BYTZ/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BYTZ
+//
+//  Created by admin on 2020-04-07.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
